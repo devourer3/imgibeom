1fc1890a7407157747fabbbcb740d2c1035968dd
beom <beom@frifee.co>
2020-03-09 14:41:16 +0900

    css modified.

--- src/components/career.js
+++ src/components/career.js
@@ -1,22 +1,22 @@
 import { useStore } from "../stores/useStore"
 import React, { useState } from "react"
 import { DATA_URL } from "../stores/base-store"
-import '../styles/career.scss'
+import "../styles/career.scss"
 
 export const Career = () => {
-  const store = useStore();
-  const [infoHover, setHover] = useState(false);
+  const store = useStore()
+  const [infoHover, setHover] = useState(false)
   const onMouseEnter = (e, listType) => {
-    e.preventDefault();
-    setHover(true);
+    e.preventDefault()
+    setHover(true)
   }
   const onMouseLeave = (e) => {
-    e.preventDefault();
-    setHover(false);
+    e.preventDefault()
+    setHover(false)
   }
   const careerRender = () => {
-    const data = store.base.data.career;
-    let view = [];
+    const data = store.base.data.career
+    let view = []
     if (data !== null) {
       data.map((item, index) => {
         view.push(
@@ -29,10 +29,12 @@ export const Career = () => {
               src={DATA_URL + item.company_image}
               alt=""
               onError={(e) => e.target.setAttribute("src", DATA_URL + "construction.png")}/>
-            <button
-              className="info-btn"
-              onMouseEnter={event => onMouseEnter(event, "-")}
-              onMouseLeave={event => onMouseLeave(event)}/>
+            <div className="info-btn-wrapper">
+              <button
+                className="info-btn"
+                onMouseEnter={event => onMouseEnter(event, "-")}
+                onMouseLeave={event => onMouseLeave(event)}/>
+            </div>
             {
               (infoHover === true) &&
               <span className="desc">{item.desc}</span>
